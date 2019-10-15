cc8763d0926277aaa5ea2f5faee81b9c1cd44523
Batosta <up201605330@g.uporto.pt>
2019-10-15 17:31:06 +0100

    Resolving initialization problems

--- README.md
+++
@@ -1,2 +0,0 @@
-# AiadProject2019
-Cgra project and intermediate deliverys developed with @batosta and @xjoaoalvesx
---
+++ src/agents/AgentTemp.java
@@ -0,0 +1,11 @@
+package agents;
+
+import jade.core.Agent;
+
+public class AgentTemp extends Agent {
+
+    protected void setup(){
+
+        System.out.println("Hello!");
+    }
+}
