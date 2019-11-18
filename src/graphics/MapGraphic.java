23cdfde861718454ce36a99da43ac0a556113874
Joao Alves <joao-carlos.alves@hotmail.com>
2019-11-18 00:40:03 +0000

    Adds interface

--- src/graphics/MapGraphic.java
+++ src/graphics/MapGraphic.java
@@ -1,28 +1,27 @@
-package app;
+package graphics;
 
-import graphics.MapGraphic;
 
 import javax.imageio.ImageIO;
 import javax.swing.*;
+import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
-import java.text.SimpleDateFormat;
-import java.util.Calendar;
+import java.util.ArrayList;
 
-public class Map extends JFrame implements Runnable{
+public class MapGraphic extends JPanel {
 
-    private int WIDTH = 700;
-    private int HEIGHT = 700;
-
+    private int WIDTH;
+    private int HEIGHT;
     private BufferedImage road;
     private BufferedImage roadh;
     private BufferedImage ground;
     private BufferedImage simple_car;
+    private BufferedImage simple_car1;
+    private BufferedImage simple_car2;
+    private BufferedImage tf;
 
-    private MapGraphic map;
-
-    public static Character[][] originalMap = new Character[][]{
+    public static Character[][] roadMap = new Character[][]{
             {' ', ' ', ' ', ' ', ' ', '|', ' ', ' ', ' ', ' ', ' ', '|', ' ', ' ', ' ', ' ', ' ', '|', ' ', ' '},
             {' ', ' ', ' ', ' ', ' ', '|', ' ', ' ', ' ', ' ', ' ', '|', ' ', ' ', ' ', ' ', ' ', '|', ' ', ' '},
             {' ', ' ', ' ', ' ', ' ', '|', ' ', ' ', ' ', ' ', ' ', '|', ' ', ' ', ' ', ' ', ' ', '|', ' ', ' '},
@@ -42,51 +41,15 @@ public class Map extends JFrame implements Runnable{
             {' ', ' ', ' ', ' ', ' ', '|', ' ', ' ', ' ', ' ', ' ', '|', ' ', ' ', ' ', ' ', ' ', '|', ' ', ' '},
             {'-', '-', '-', '-', '-', '+', '-', '-', '-', '-', '-', '+', '-', '-', '-', '-', '-', '+', '-', '-'},
             {' ', ' ', ' ', ' ', ' ', '|', ' ', ' ', ' ', ' ', ' ', '|', ' ', ' ', ' ', ' ', ' ', '|', ' ', ' '},
-            {' ', ' ', ' ', ' ', ' ', '|', ' ', ' ', ' ', ' ', ' ', '|', ' ', ' ', ' ', ' ', ' ', '|', ' ', ' '}
+            {' ', ' ', ' ', ' ', ' ', '|', ' ', ' ', ' ', ' ', ' ', '|', ' ', ' ', ' ', ' ', ' ', '|', ' ', ' '},
     };
 
-    public static Character[][] oldMap = new Character[originalMap[0].length][originalMap.length];
-    public static Character[][] newMap = new Character[originalMap[0].length][originalMap.length];
 
-    Map(){
-        copyMap(originalMap, oldMap);
-        copyMap(originalMap, newMap);
+    public MapGraphic(){
+        WIDTH = 700;
+        HEIGHT = 700;
 
-        map = new MapGraphic();
-        this.getContentPane().add(map);
-        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        this.setSize(WIDTH, HEIGHT);
-        this.setVisible(true);
-    }
-
-    @Override
-    public void run() {
-//        Calendar cal = Calendar.getInstance();
-//        SimpleDateFormat sdf = new SimpleDateFormat("HH:mm:ss:SS");
-//        System.out.println( "Map: " + sdf.format(cal.getTime()) );
-        map.setMap(newMap);
-        for(Character[] line : newMap) {
-            for (Character pos : line) {
-                System.out.print(pos);
-                System.out.print(' ');
-            }
-            System.out.print('\n');
-        }
-        System.out.print('\n');
-        copyMap(newMap, oldMap);
-        copyMap(originalMap, newMap);
-
-
-
-//        cal = Calendar.getInstance();
-//        sdf = new SimpleDateFormat("HH:mm:ss:SS");
-//        System.out.println( "Map_end: " + sdf.format(cal.getTime()) );
-    }
-
-    private void copyMap(Character[][] from, Character[][] to){
-        for (int r = 0; r < from.length; r++) {
-            to[r] = from[r].clone();
-        }
+        loadAssets();
     }
 
     public void loadAssets(){
@@ -94,15 +57,63 @@ public class Map extends JFrame implements Runnable{
         File file1 = new File("./resources/roadh.jpg");
         File file2 = new File("./resources/ground.jpg");
         File file3 = new File("./resources/simple_car.jpg");
+        File file4 = new File("./resources/simple_car1.jpg");
+        File file5 = new File("./resources/simple_car2.jpg");
+        File file6 = new File("./resources/tf.jpg");
 
         try{
-            this.road = ImageIO.read(file);
-            this.roadh = ImageIO.read(file1);
-            this.ground = ImageIO.read(file2);
-            this.simple_car = ImageIO.read(file3);
+            road = ImageIO.read(file);
+            roadh = ImageIO.read(file1);
+            ground = ImageIO.read(file2);
+            simple_car = ImageIO.read(file3);
+            simple_car1 = ImageIO.read(file4);
+            simple_car2 = ImageIO.read(file5);
+            tf = ImageIO.read(file6);
+
         }catch(IOException e){
             e.printStackTrace();
         }
 
     }
+
+    protected void paintComponent(Graphics g){
+        super.paintComponents(g);
+        for(int i = 0; i < roadMap.length; i ++){
+            for(int j = 0; j < roadMap[i].length; j++){
+                switch(roadMap[j][i]){
+                    case '|':
+                        g.drawImage(road, i*road.getWidth(), j*road.getHeight(), null);
+                        break;
+
+                    case '-':
+                        g.drawImage(roadh, i*roadh.getWidth(), j*roadh.getHeight(), null);
+                        break;
+
+                    case ' ':
+                        g.drawImage(ground, i*ground.getWidth(), j*ground.getHeight(), null);
+                        break;
+
+                    case 'X':
+                        if(j == 5 || j==11 || j== 17){
+                            g.drawImage(simple_car, i*simple_car.getWidth(), j*simple_car.getHeight(), null);
+                        }
+                        else {
+                            g.drawImage(simple_car1, i*simple_car1.getWidth(), j*simple_car1.getHeight(), null);
+                        }
+
+                        break;
+                    case '+':
+                        g.drawImage(tf, i*tf.getWidth(), j*tf.getHeight(), null);
+                        break;
+                }
+
+            }
+        }
+    }
+
+    public void setMap(Character[][] m){
+        this.roadMap = m;
+        repaint();
+    }
+
 }
